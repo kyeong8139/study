d7a819b94a1737045b796ad024672bc2952c3fa3
kyeong8139 <kyeong8139@gmail.com>
2025-05-07 15:14:16 +0900

    refactor: factory method pattern

--- Springbook/src/main/java/springbook/user/dao/UserDao.java
+++ Springbook/src/main/java/springbook/user/dao/UserDao.java
@@ -4,7 +4,7 @@ import springbook.user.domain.User;
 
 import java.sql.*;
 
-public class UserDao {
+public abstract class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
         Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement(
@@ -39,10 +39,5 @@ public class UserDao {
         return user;
     }
 
-    private Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("org.mariadb.jdbc.Driver");
-        Connection c = DriverManager.getConnection
-                ("jdbc:mariadb://localhost:3306/springbook", "root", "root");
-        return c;
-    }
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
 }
