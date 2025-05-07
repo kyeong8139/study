1758655d5b125c8d7e208c6a5c3c915d8251471d
kyeong8139 <kyeong8139@gmail.com>
2025-05-07 15:01:57 +0900

    refactor: extract method

--- Springbook/src/main/java/springbook/user/dao/UserDao.java
+++ Springbook/src/main/java/springbook/user/dao/UserDao.java
@@ -5,10 +5,8 @@ import springbook.user.domain.User;
 import java.sql.*;
 
 public class UserDao {
-    public void add (User user) throws ClassNotFoundException, SQLException {
-        Class.forName("org.mariadb.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mariadb://localhost:3306/springbook", "root", "root");
-
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values(?, ?, ?)");
         ps.setString(1, user.getId());
@@ -21,10 +19,8 @@ public class UserDao {
         c.close();
     }
 
-    public User get (String id) throws ClassNotFoundException, SQLException {
-        Class.forName("org.mariadb.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mariadb://localhost:3306/springbook", "root", "root");
-
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id = ?");
         ps.setString(1, id);
@@ -41,5 +37,12 @@ public class UserDao {
         c.close();
 
         return user;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        Connection c = DriverManager.getConnection
+                ("jdbc:mariadb://localhost:3306/springbook", "root", "root");
+        return c;
     }
 }
