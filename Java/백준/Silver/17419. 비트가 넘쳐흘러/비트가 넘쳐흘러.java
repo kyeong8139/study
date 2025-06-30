094e14e126f6df87f311c3798435a69915fa52fb
bokyeong-Lee <149753416+kyeong8139@users.noreply.github.com>
2025-07-01 08:54:18 +0900

    [Silver IV] Title: 비트가 넘쳐흘러, Time: 220 ms, Memory: 20032 KB, Score: 100 point -BaekjoonHub

--- Java/백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.java
+++ Java/백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.java
@@ -9,40 +9,13 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         String k = br.readLine();
 
-        int cnt = 0;
-        String num = k;
-        do {
-            num = calcurate(num);
-            cnt++;
-        } while (!num.equals("0"));
-
-        System.out.print(cnt);
-    }
-
-    public static String calcurate (String num) {
-    
-        char[] origin = num.toCharArray();
-        char[] result = num.toCharArray();
-        for (int i = 0; i < result.length; i++) {
-            result[i] = (result[i] == '0') ? '1' : '0';
-        }
-
-        for (int i = result.length - 1; i >= 0; i--) {
-            if (result[i] == '0') {
-                result[i] = '1';
-                break;
-            } else {
-                result[i] = '0';
+        int answer = 0;
+        for (int i = 0; i < k.length(); i++) {
+            if (k.charAt(i) == '1') {
+                answer++;
             }
         }
 
-        for (int i = 0; i < result.length; i++) {
-            if (origin[i] != result[i]) {
-                result[i] = '0';
-            }
-        }
-
-        BigInteger answer = new BigInteger(new String(origin), 2).subtract(new BigInteger(new String(result), 2));
-        return answer.toString(2);
+        System.out.println(answer);
     }
 }
