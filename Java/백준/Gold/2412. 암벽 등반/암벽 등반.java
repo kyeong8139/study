887ed2a82c436cb6a97bc84cd26be3b7d354597a
bokyeong-Lee <149753416+kyeong8139@users.noreply.github.com>
2025-07-03 00:59:53 +0900

    [Gold IV] Title: 암벽 등반, Time: 1456 ms, Memory: 39468 KB -BaekjoonHub

---
+++ Java/백준/Gold/2412. 암벽 등반/암벽 등반.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static class Point{
+        int id;
+        int x;
+        int y;
+        int dist;
+
+        public Point(int id, int x, int y, int dist) {
+            this.id = id;
+            this.x = x;
+            this.y = y;
+            this.dist = dist;
+        }
+    } 
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int goal = Integer.parseInt(st.nextToken());
+
+        int[] dist = new int[n+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        PriorityQueue<Point> pq = new PriorityQueue<>((x, y) -> x.dist - y.dist);
+        HashMap<Integer, List<Point>> points = new HashMap<>();
+        points.put(0, new ArrayList<>());
+        points.get(0).add(new Point(0, 0, 0, 0));
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            Point cur = new Point(i, x, y, 0);
+
+            if (!points.containsKey(x)) {
+                points.put(x, new ArrayList<>());
+            }
+            points.get(x).add(cur);
+
+            if (y == goal) {
+                dist[cur.id] = 0;
+                pq.add(cur);
+            }
+        }
+
+        while(!pq.isEmpty()) {
+            Point cur = pq.poll();
+            if (dist[cur.id] < cur.dist) continue;
+            
+            if (cur.x == 0 && cur.y == 0) break;
+
+            for (int i = cur.x - 2; i <= cur.x + 2; i++) {
+                List<Point> nexts = points.get(i);
+                if (nexts == null) continue;
+                
+                for (Point next : nexts) {
+                    if ((Math.abs(cur.y-next.y) <= 2) && (dist[next.id] > cur.dist + 1)) {
+                        dist[next.id] = cur.dist + 1;
+                        pq.add(new Point(next.id, next.x, next.y, cur.dist + 1));
+                    }
+                }
+            }
+        }
+
+        int answer = dist[0] == Integer.MAX_VALUE ? -1 : dist[0];
+        System.out.print(answer);
+    }
+}   
