2721bc855e695c8289cd3abf02bf9a34bff2cf1a
bokyeong-Lee <149753416+kyeong8139@users.noreply.github.com>
2025-07-10 15:41:07 +0900

    [Platinum V] Title: 행성 터널, Time: 1344 ms, Memory: 97024 KB -BaekjoonHub

---
+++ Java/백준/Platinum/2887. 행성 터널/행성 터널.java
@@ -0,0 +1,116 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class Edge implements Comparable<Edge> {
+        int to;
+        int dist;
+
+        public Edge (int to, int dist) {
+            this.to = to;
+            this.dist = dist;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.dist - o.dist;
+        }
+    }
+
+    static class Planet {
+        int id;
+        int[] pos;
+
+        public Planet(int id, int[] pos) {
+            this.id = id;
+            this.pos = pos;
+        }
+    }
+
+    static TreeSet<Planet>[] planets;
+    static int[] parents;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        Map<Integer, Planet> planetMap = new HashMap<>();
+        planets = new TreeSet[3];
+        planets[0] = new TreeSet<>((x, y) -> (x.pos[0] == y.pos[0]) ? x.id - y.id : x.pos[0] - y.pos[0]);
+        planets[1] = new TreeSet<>((x, y) -> (x.pos[1] == y.pos[1]) ? x.id - y.id : x.pos[1] - y.pos[1]);
+        planets[2] = new TreeSet<>((x, y) -> (x.pos[2] == y.pos[2]) ? x.id - y.id : x.pos[2] - y.pos[2]);
+
+        for (int id = 1; id <= n; id++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int z = Integer.parseInt(st.nextToken());
+
+            Planet planet = new Planet(id, new int[]{x, y, z});
+            planets[0].add(planet);
+            planets[1].add(planet);
+            planets[2].add(planet);
+            planetMap.put(id, planet);
+        }
+
+        parents = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            parents[i] = i;
+        }
+
+        Planet cur = planets[0].first();
+        parents[cur.id] = 0;
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        addEdge(cur, pq);
+
+        int cnt = 1;
+        long distSum = 0;
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
+            if (parents[edge.to] == 0) continue;
+
+            distSum += edge.dist;
+            parents[edge.to] = 0;
+            addEdge(planetMap.get(edge.to), pq);
+            
+            if (++cnt == n) break;
+        }
+
+        System.out.print(distSum);
+    }
+
+    public static void addEdge(Planet cur, PriorityQueue<Edge> pq) {
+        for (int i = 0; i < 3; i++) {
+            TreeSet<Planet> set = planets[i];
+            set.remove(cur);
+                
+            cur.id = 0;
+            Planet near = set.ceiling(cur);
+            while (near != null) {
+                if (parents[near.id] != 0) {
+                    int nearDist = Math.abs(cur.pos[i] - near.pos[i]);
+                    pq.add(new Edge(near.id, nearDist));
+                    break;
+                }
+
+                set.remove(near);
+                near = set.ceiling(near);
+            }
+
+            cur.id = 10_0001;
+            near = set.floor(cur);
+            while (near != null) {
+                if (parents[near.id] != 0) {
+                    int nearDist = Math.abs(cur.pos[i] - near.pos[i]);
+                    pq.add(new Edge(near.id, nearDist));
+                    break;
+                }
+
+                set.remove(near);
+                near = set.floor(near);
+            }
+        }
+    }
+}
