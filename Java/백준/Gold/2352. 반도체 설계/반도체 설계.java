af2251b3075f54f9fe8018dcc63231839407d56a
bokyeong-Lee <149753416+kyeong8139@users.noreply.github.com>
2025-07-15 19:49:48 +0900

    [Gold II] Title: 반도체 설계, Time: 232 ms, Memory: 20656 KB -BaekjoonHub

---
+++ Java/백준/Gold/2352. 반도체 설계/반도체 설계.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int[] values; 
+    static int length = 0;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        values = new int[n];
+        for (int i = 0; i < n; i++) {
+            int idx = binarySearch(0, length, arr[i]);
+            if (idx == length) length++;
+            values[idx] = arr[i];
+        }
+
+        System.out.print(length);
+    }
+
+    private static int binarySearch(int start, int end, int target) {
+        int low = start;
+        int high = end;
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+
+            if (values[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+}   
