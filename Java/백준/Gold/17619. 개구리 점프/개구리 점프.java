3b0b3c81f8aee0ecac4d621e64a5b8217244fedc
bokyeong-Lee <149753416+kyeong8139@users.noreply.github.com>
2025-07-18 22:07:20 +0900

    [Gold III] Title: 개구리 점프, Time: 920 ms, Memory: 98360 KB, Score: 100 point -BaekjoonHub

--- Java/백준/Gold/17619. 개구리 점프/개구리 점프.java
+++ Java/백준/Gold/17619. 개구리 점프/개구리 점프.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Main {
 
-    static class Block implements Comparable<Block>{
+    static class Block{
         int id;
         int xStart;
         int xEnd;
@@ -15,14 +15,6 @@ public class Main {
             this.xEnd = xEnd;
             this.y = y;
         }
-
-        @Override
-        public int compareTo(Block o) {
-            if (this.xStart == o.xStart) {
-                return this.xEnd - o.xEnd;
-            }
-            return this.xStart - o.xStart;
-        }
     }
 
     static int[] parents;
@@ -33,33 +25,51 @@ public class Main {
 
         int n = Integer.parseInt(st.nextToken());
         int q = Integer.parseInt(st.nextToken());
-        TreeSet<Block> blocks = new TreeSet<>();
-        for (int id = 1; id <= n; id++) {
+        Block[] blocks = new Block[n];
+        for (int idx = 0; idx < n; idx++) {
             st = new StringTokenizer(br.readLine());
+            int id = idx+1;
             int xStart = Integer.parseInt(st.nextToken());
             int xEnd = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             
             Block block = new Block(id, xStart, xEnd, y);
-            blocks.add(block);
+            blocks[idx] = block;
         }
+        Arrays.sort(blocks, (x, y) -> x.xStart - y.xStart);
         
         parents = new int[n+1];
         for (int i = 1; i <= n; i++) {
             parents[i] = i;
         }
         
-        while (!blocks.isEmpty()) {
-            Block prev = blocks.pollFirst();
-            Block cur = blocks.higher(prev);
-            while(!blocks.isEmpty() && cur != null) {
-                if (prev.xEnd < cur.xStart) break;
-    
-                union(cur, prev);
-                cur = blocks.higher(cur);
+        PriorityQueue<Block> pq = new PriorityQueue<>((x, y) -> x.xEnd - y.xEnd);
+        for (int i = 0; i < n; i++) {
+            Block cur = blocks[i];
+            
+            // 가지치기
+            while(!pq.isEmpty() && pq.peek().xEnd < cur.xStart) {
+                pq.poll();
             }
+            
+            Block result = new Block(cur.id, cur.xStart, cur.xEnd, cur.y);
+            /**
+             * Block near = pq.poll()일 때, near은 항상 아래 조건을 만족함
+             *      - near.xStart <= cur.xStart <= near.xEnd
+             */
+            while(!pq.isEmpty()) { 
+                Block near = pq.poll();
+                
+                int curParent = getParent(cur.id);
+                int nearParent = getParent(near.id);
 
-            blocks.remove(prev);
+                if (curParent != nearParent) {
+                    parents[nearParent] = curParent;
+                    result.xStart = Math.min(result.xStart, near.xStart);
+                    result.xEnd = Math.max(result.xEnd, near.xEnd);
+                }
+            }
+            pq.add(result);
         }
 
         StringBuilder sb = new StringBuilder();
@@ -68,25 +78,16 @@ public class Main {
             int blockA = Integer.parseInt(st.nextToken());
             int blockB = Integer.parseInt(st.nextToken());
 
-            int result = 1;
+            boolean isMovable = false;
             int parentA = getParent(blockA);
             int parentB = getParent(blockB);
-            if (parentA != parentB) {
-                result = 0;
+            if (parentA == parentB) {
+                isMovable = true;
             }
 
-            sb.append(result).append("\n");
+            sb.append(isMovable ? 1 : 0).append("\n");
         }
         System.out.print(sb);
-    }
-
-    public static void union(Block prev, Block cur) {
-        int prevParent = getParent(prev.id);
-        int curParent = getParent(cur.id);
-
-        if (prevParent != curParent) {
-            parents[curParent] = prevParent;
-        }
     }
 
     public static int getParent(int child) {
