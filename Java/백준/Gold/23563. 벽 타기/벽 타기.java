c447665ba79ce4d90e9ba166d43186f31d3245f6
bokyeong-Lee <149753416+kyeong8139@users.noreply.github.com>
2025-09-09 23:57:52 +0900

    [Gold III] Title: 벽 타기, Time: 532 ms, Memory: 50520 KB -BaekjoonHub

---
+++ Java/백준/Gold/23563. 벽 타기/벽 타기.java
@@ -0,0 +1,89 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static class Pos implements Comparable<Pos> {
+        int row;
+        int col;
+        int time;
+
+        public Pos(int row, int col, int time) {
+            this.row = row;
+            this.col = col;
+            this.time = time;
+        }
+
+        public int compareTo (Pos o) {
+            return this.time - o.time;
+        }
+    }
+
+    static int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int h = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        char[][] board = new char[h][w];
+        for (int r = 0; r < h; r++) {
+            board[r] = br.readLine().toCharArray();
+        }
+
+        PriorityQueue<Pos> pq = new PriorityQueue<>();
+        for (int r = 0; r < h; r++) {
+            for (int c = 0; c < w; c++) {
+                if (board[r][c] == 'S') {
+                    pq.add(new Pos(r, c, 0));
+                }
+            }
+        }
+
+        int[][] times = new int[h][w];
+        boolean[][] wallIsNear = new boolean[h][w];
+        for (int r = 0; r < h; r++) {
+            Arrays.fill(times[r], Integer.MAX_VALUE);
+
+            for (int c = 0; c < w; c++) {
+                if (board[r][c] == '#') {
+                    for (int[] dir : dirs) {
+                       int nextRow = r + dir[0];
+                        int nextCol = c + dir[1];
+                        if (nextRow < 0 || nextRow >= h || nextCol < 0 || nextCol >= w) continue;
+                        wallIsNear[nextRow][nextCol] = true;
+                    }
+                }
+                
+            }
+        }
+
+        while (!pq.isEmpty()) {
+            Pos cur = pq.poll();
+            if (times[cur.row][cur.col] <= cur.time) continue;
+
+            times[cur.row][cur.col] = cur.time;
+            
+            for (int[] dir : dirs) {
+                int nextRow = cur.row + dir[0];
+                int nextCol = cur.col + dir[1];
+
+                if (nextRow < 0 || nextRow >= h || nextCol < 0 || nextCol >= w || (board[nextRow][nextCol] == '#')) continue;
+
+                int nextTime = cur.time;
+                if (!wallIsNear[cur.row][cur.col] || !wallIsNear[nextRow][nextCol]) {
+                    nextTime++;
+                }
+                pq.add(new Pos(nextRow, nextCol, nextTime));
+            }
+        }
+
+        for (int r = 0; r < h; r++) {
+            for (int c = 0; c < w; c++) {
+                if (board[r][c] == 'E') {
+                    System.out.println(times[r][c]);
+                    break;
+                }
+            }
+        }
+    }
+}
